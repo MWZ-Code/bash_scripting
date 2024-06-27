c419d952b781922771ce43403ad134980acae2b9
Mak Wei Zheng <64359863+Mak-Wei-Zheng@users.noreply.github.com>
2024-06-27 17:46:01 +0800

    adding in scripts for logging cpu information

---
+++ turbo_log.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export TZ="Asia/Singapore"
+# Change to the desired directory
+cd ~/ceremonyclient/node
+
+# Log the current time
+echo "Log Time: $(date)" >> ~/ceremonyclient/node/turbo.log
+
+# Execute the command and append the output to the log file
+turbostat --Summary --num_iterations 1>> ~/ceremonyclient/node/turbo.log 2>&1
+
+echo "____LINE__BREAK____" >> ~/ceremonyclient/node/turbo.log 2>&1
